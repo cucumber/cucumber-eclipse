35164c22dbd8bfade398c6a2f9257ac03a4fd8b2
Ilan Pillemer <ilan.pillemer@gmail.com>
2013-01-04 16:55:15 +0200

    Added extension point (plugin.xml) and icon for feature files

---
+++ cucumber.eclipse.editor/src/main/java/cucumber/eclipse/editor/editors/Editor.java
@@ -0,0 +1,14 @@
+package cucumber.eclipse.editor.editors;
+
+import org.eclipse.ui.editors.text.TextEditor;
+
+public class Editor extends TextEditor {
+
+	public Editor() {
+		super();
+	}
+	public void dispose() {
+		super.dispose();
+	}
+
+}
