7a53112d0225a68734e6a7fedbe742929f9f11c4
Ilan Pillemer <ilan.pillemer@gmail.com>
2013-01-12 22:35:36 +0200

    Got pretty formatting working correctly fixes #10
    Switched to viewwerConfiguration effectively moving the context menu to
    the editors context menu rather than the file systems context menu.
    Autoformatting now works.. could still use a short cut, will create a
    new issue for that.

--- cucumber.eclipse.editor/src/main/java/cucumber/eclipse/editor/editors/GherkinConfiguration.java
+++ cucumber.eclipse.editor/src/main/java/cucumber/eclipse/editor/editors/GherkinConfiguration.java
@@ -2,14 +2,11 @@ package cucumber.eclipse.editor.editors;
 
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.TextAttribute;
-import org.eclipse.jface.text.formatter.IContentFormatter;
-import org.eclipse.jface.text.formatter.MultiPassContentFormatter;
 import org.eclipse.jface.text.presentation.IPresentationReconciler;
 import org.eclipse.jface.text.presentation.PresentationReconciler;
 import org.eclipse.jface.text.rules.DefaultDamagerRepairer;
 import org.eclipse.jface.text.rules.Token;
 import org.eclipse.jface.text.source.ISourceViewer;
-import org.eclipse.jface.text.source.SourceViewerConfiguration;
 import org.eclipse.ui.editors.text.TextSourceViewerConfiguration;
 
 public class GherkinConfiguration extends TextSourceViewerConfiguration {
--- cucumber.eclipse.editor/src/main/java/cucumber/eclipse/editor/editors/GherkinPopupMenuPrettyFormatActionDelegate.java
+++ cucumber.eclipse.editor/src/main/java/cucumber/eclipse/editor/editors/GherkinPopupMenuPrettyFormatActionDelegate.java
@@ -1,79 +1,61 @@
 package cucumber.eclipse.editor.editors;
 
+import gherkin.lexer.LexingError;
 import gherkin.parser.ParseError;
 
-import org.eclipse.core.resources.IFile;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.jface.viewers.TreeSelection;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.IEditorActionDelegate;
 import org.eclipse.ui.IEditorPart;
-import org.eclipse.ui.IObjectActionDelegate;
-import org.eclipse.ui.IWorkbenchPart;
-import org.eclipse.ui.texteditor.IDocumentProvider;
 import org.eclipse.ui.texteditor.ITextEditor;
 import cucumber.eclipse.editor.Activator;
 
-
-
 public class GherkinPopupMenuPrettyFormatActionDelegate implements
-		IObjectActionDelegate {
+		IEditorActionDelegate {
 
-	private Shell shell;
-	private IFile docSelected;
-	
 	@Override
-	public void selectionChanged(IAction arg0, ISelection arg1) {
-		if (arg1 instanceof TreeSelection) {
-			TreeSelection tree = (TreeSelection) arg1;
-			docSelected = (IFile) tree.getFirstElement();
+	public void selectionChanged(IAction action, ISelection selection) {
+
+	}
+
+	@Override
+	public void run(IAction action) {
+
+		IEditorPart editorPart = Activator.getDefault().getWorkbench()
+				.getActiveWorkbenchWindow().getActivePage().getActiveEditor();
+		ITextEditor editor = (ITextEditor) editorPart;
+		Shell shell = editorPart.getSite().getShell();
+		IDocument doc = editor.getDocumentProvider().getDocument(editor.getEditorInput());
+		String contents = doc.get();
+		try {
+			String formatted = GherkinFormatterUtil.format(contents);
+			doc.replace(0, doc.getLength(), formatted);
+		} catch (ParseError e) {
+			MessageDialog.openInformation(shell, "Unable to pretty format.",
+					"One can only format a feature file that has no parse errors: \n"
+							+ "The following parse error was encountered: ["
+							+ e.getMessage() + "]");
+
+		} catch (LexingError e) {
+			MessageDialog.openInformation(shell, "Unable to pretty format.",
+					"One can only format a feature file that has no lexing errors: \n"
+							+ "The following lex error was encountered: ["
+							+ e.getMessage() + "]");
+		}
+		
+		catch (BadLocationException e) {
+			e.printStackTrace();
 		}
 
 	}
 
 	@Override
-	public void setActivePart(IAction arg0, IWorkbenchPart targetPart) {
-		shell = targetPart.getSite().getShell();
+	public void setActiveEditor(IAction action, IEditorPart part) {
 
-	}
-
-	@Override
-	public void run(IAction arg0) {
-		
-		IEditorPart editorPart = Activator.getDefault().getWorkbench().getActiveWorkbenchWindow().getActivePage().getActiveEditor();
-        ITextEditor editor = (ITextEditor) editorPart;
-        IDocumentProvider dp = editor.getDocumentProvider();
-        IDocument doc = dp.getDocument(editor.getEditorInput());
-        
-        String title = Activator.getDefault().getWorkbench().getActiveWorkbenchWindow().getActivePage().getActivePartReference().getTitle();
-        if (title.equals(docSelected.getName())) {
-            String contents = doc.get();
-            
-    		String formatted;
-    		try {
-    			formatted = GherkinFormatterUtil.format(contents);
-    			doc.replace(0, doc.getLength(), formatted);
-    		} catch (ParseError e) {
-    			MessageDialog.openInformation(shell, "Unable to pretty format.", 
-    					"One can only format a feature file that has no parse errors: \n"
-    					+ "The following parse error was encountered: [" + e.getMessage() +"]");
-    			
-    		} catch (BadLocationException e) {
-    			e.printStackTrace();
-    		}
-
-        } else {
-        	MessageDialog.openInformation(shell, "This is still Alpha...", 
-					"One can only format a feature file from the popup menu if... \n"
-					+ ".. it is the active editor... sorry.");
-        }
-        
-   
-
-		
 	}
 
 }
