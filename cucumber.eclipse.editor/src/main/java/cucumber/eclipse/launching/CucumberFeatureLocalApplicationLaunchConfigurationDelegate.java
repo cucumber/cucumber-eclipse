11738dd272247714b157712490d087ae74577184
Ilan Pillemer <ilan.pillemer@gmail.com>
2013-05-08 01:00:29 +0200

    formatted code

--- cucumber.eclipse.editor/src/main/java/cucumber/eclipse/launching/CucumberFeatureLocalApplicationLaunchConfigurationDelegate.java
+++ cucumber.eclipse.editor/src/main/java/cucumber/eclipse/launching/CucumberFeatureLocalApplicationLaunchConfigurationDelegate.java
@@ -17,55 +17,36 @@ import org.eclipse.jdt.launching.IVMInstall;
 import org.eclipse.jdt.launching.IVMRunner;
 import org.eclipse.jdt.launching.VMRunnerConfiguration;
 
-public class CucumberFeatureLocalApplicationLaunchConfigurationDelegate
-extends AbstractJavaLaunchConfigurationDelegate
-		implements ILaunchConfigurationDelegate2 {
-
+public class CucumberFeatureLocalApplicationLaunchConfigurationDelegate extends AbstractJavaLaunchConfigurationDelegate implements ILaunchConfigurationDelegate2 {
 
 	@Override
-	public void launch(ILaunchConfiguration config, String mode, ILaunch launch,
-			IProgressMonitor monitor) throws CoreException {
-		
+	public void launch(ILaunchConfiguration config, String mode, ILaunch launch, IProgressMonitor monitor) throws CoreException {
+
 		IVMInstall vm = verifyVMInstall(config);
 		IVMRunner runner = vm.getVMRunner(mode);
-		String[] classpath= getClasspath(config);   
-		VMRunnerConfiguration runConfig = new VMRunnerConfiguration("cucumber.api.cli.Main" , classpath);
+		String[] classpath = getClasspath(config);
+		VMRunnerConfiguration runConfig = new VMRunnerConfiguration("cucumber.api.cli.Main", classpath);
 
-		 
-		
-		
-//		runConfig.setProgramArguments(new String[] {buildHTMLFile(configuration)});
-//		    String[] vmArgs = execArgs.getVMArgumentsArray();
-//		    String[] realArgs = new String[vmArgs.length+1];
-//		    System.arraycopy(vmArgs, 0, realArgs, 1, vmArgs.length);
-//		    realArgs[0] = javaPolicy;
-//		 runConfig.setVMArguments(realArgs);
 		File workingDir = verifyWorkingDirectory(config);
 		String workingDirName = null;
 		if (workingDir != null) {
-			workingDirName= workingDir.getAbsolutePath();
+			workingDirName = workingDir.getAbsolutePath();
 		}
-		    runConfig.setWorkingDirectory(workingDirName);
-		    // Bootpath
-		    String[] bootpath = getBootpath(config);
-		 runConfig.setBootClassPath(bootpath);
-		 String featurePath = "/Users/ilanpillemer/Developer/Projects/cucumber-jvm/examples/java-calculator/src/test/resources/cucumber/examples/java/calculator/basic_arithmetic.feature";	
-		 String glue = "--glue";
-		 String gluePath = "cucumber.examples.java.calculator";
-		 String[] args = new String[3];
-		 
-		 args[0] = featurePath;
-		 args[1] = glue;
-		 args[2] = gluePath;
-		 runConfig.setProgramArguments(args);
-		 
-		   
-		    // Launch the configuration
-		 //   this.fCurrentLaunchConfiguration = configuration;
-		 runner.run(runConfig, launch, monitor);
-		
-	}
-	
 
+		String[] bootpath = getBootpath(config);
+		runConfig.setBootClassPath(bootpath);
+		String featurePath = "/Users/ilanpillemer/Developer/Projects/cucumber-jvm/examples/java-calculator/src/test/resources/cucumber/examples/java/calculator/basic_arithmetic.feature";
+		String glue = "--glue";
+		String gluePath = "cucumber.examples.java.calculator";
+		String[] args = new String[3];
+
+		args[0] = featurePath;
+		args[1] = glue;
+		args[2] = gluePath;
+		runConfig.setProgramArguments(args);
+
+		runner.run(runConfig, launch, monitor);
+
+	}
 
 }
--- cucumber.eclipse.editor/src/main/java/cucumber/eclipse/launching/CucumberMainTab.java
+++ cucumber.eclipse.editor/src/main/java/cucumber/eclipse/launching/CucumberMainTab.java
@@ -1,57 +1,21 @@
 package cucumber.eclipse.launching;
 
-import java.lang.reflect.InvocationTargetException;
-import java.util.List;
-
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IResource;
-import org.eclipse.core.resources.ResourcesPlugin;
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IAdaptable;
-import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
 import org.eclipse.debug.internal.ui.SWTFactory;
-import org.eclipse.debug.ui.ILaunchConfigurationDialog;
 import org.eclipse.debug.ui.ILaunchConfigurationTab;
-import org.eclipse.jdt.core.IJavaElement;
-import org.eclipse.jdt.core.IJavaModel;
-import org.eclipse.jdt.core.IJavaProject;
-import org.eclipse.jdt.core.IType;
-import org.eclipse.jdt.core.JavaCore;
-import org.eclipse.jdt.core.JavaModelException;
-import org.eclipse.jdt.core.search.IJavaSearchScope;
-import org.eclipse.jdt.core.search.SearchEngine;
-import org.eclipse.jdt.debug.ui.launchConfigurations.JavaMainTab;
-import org.eclipse.jdt.internal.debug.ui.IJavaDebugHelpContextIds;
 import org.eclipse.jdt.internal.debug.ui.JDIDebugUIPlugin;
-import org.eclipse.jdt.internal.debug.ui.launcher.DebugTypeSelectionDialog;
-import org.eclipse.jdt.internal.debug.ui.launcher.LauncherMessages;
-import org.eclipse.jdt.internal.debug.ui.launcher.MainMethodSearchEngine;
 import org.eclipse.jdt.internal.debug.ui.launcher.SharedJavaMainTab;
 import org.eclipse.jdt.launching.IJavaLaunchConfigurationConstants;
-import org.eclipse.jdt.launching.IVMInstall;
-import org.eclipse.jdt.launching.IVMRunner;
-import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.jface.window.Window;
-import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Control;
-import org.eclipse.swt.widgets.Text;
-import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IFileEditorInput;
 import org.eclipse.ui.IWorkbenchPage;
-import org.eclipse.ui.PlatformUI;
 
-public class CucumberMainTab  
-extends SharedJavaMainTab
-implements ILaunchConfigurationTab {
+public class CucumberMainTab extends SharedJavaMainTab implements ILaunchConfigurationTab {
 
 	@Override
 	public void createControl(Composite parent) {
@@ -59,7 +23,7 @@ implements ILaunchConfigurationTab {
 		createProjectEditor(comp);
 		setControl(comp);
 		IProject project = getProject();
-		if (project!=null) {
+		if (project != null) {
 			fProjText.setText(getProject().getName());
 		}
 	}
@@ -72,64 +36,56 @@ implements ILaunchConfigurationTab {
 	@Override
 	public void performApply(ILaunchConfigurationWorkingCopy config) {
 		config.setAttribute(IJavaLaunchConfigurationConstants.ATTR_PROJECT_NAME, fProjText.getText().trim());
-        mapResources(config);
-		
+		mapResources(config);
+
 	}
 
-	
 	protected IProject getProject() {
-		
-        IWorkbenchPage page = JDIDebugUIPlugin.getActivePage();
-        if (page != null) {
-          IEditorPart part = page.getActiveEditor();
-        			if(part  != null) {
-        			    IFileEditorInput input = (IFileEditorInput)part.getEditorInput() ;
-        			    IFile file = input.getFile();
-        			    IProject activeProject = file.getProject();
-        			    return activeProject;
-        			}
-        }
 
-       return null;
-    }
-	
+		IWorkbenchPage page = JDIDebugUIPlugin.getActivePage();
+		if (page != null) {
+			IEditorPart part = page.getActiveEditor();
+			if (part != null) {
+				IFileEditorInput input = (IFileEditorInput) part.getEditorInput();
+				IFile file = input.getFile();
+				IProject activeProject = file.getProject();
+				return activeProject;
+			}
+		}
+
+		return null;
+	}
+
 	@Override
 	public void setDefaults(ILaunchConfigurationWorkingCopy config) {
-	
-	     IProject javaProject = getProject();
-	        if (javaProject!= null) {
-	       	 initializeJavaProject(javaProject, config);
-	        }
-	        else {
-	         config.setAttribute(IJavaLaunchConfigurationConstants.ATTR_PROJECT_NAME, EMPTY_STRING);
-	        }
-	        
+
+		IProject javaProject = getProject();
+		if (javaProject != null) {
+			initializeJavaProject(javaProject, config);
+		} else {
+			config.setAttribute(IJavaLaunchConfigurationConstants.ATTR_PROJECT_NAME, EMPTY_STRING);
+		}
+
 	}
 
 	@Override
 	protected void handleSearchButtonSelected() {
-		
+
 	}
-	
+
 	@Override
-	public void initializeFrom(ILaunchConfiguration config) {		
+	public void initializeFrom(ILaunchConfiguration config) {
 
 	}
-	
-	   protected void initializeJavaProject(IProject javaProject, ILaunchConfigurationWorkingCopy config) {
-	        
-	        String name = null;
-	        if (javaProject != null && javaProject.exists()) {
-	            name = javaProject.getName();
-	        }
-	        config.setAttribute(IJavaLaunchConfigurationConstants.ATTR_PROJECT_NAME, name);
-	        
-	    } 
 
+	protected void initializeJavaProject(IProject javaProject, ILaunchConfigurationWorkingCopy config) {
 
+		String name = null;
+		if (javaProject != null && javaProject.exists()) {
+			name = javaProject.getName();
+		}
+		config.setAttribute(IJavaLaunchConfigurationConstants.ATTR_PROJECT_NAME, name);
 
-
+	}
 
 }
-
-
