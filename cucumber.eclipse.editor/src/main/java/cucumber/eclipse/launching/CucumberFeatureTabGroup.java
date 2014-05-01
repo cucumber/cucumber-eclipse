52638c7308314feea72de49a9437a43bddb53dcb
mark <mark@tranquillussoftware.co.uk>
2014-05-01 06:52:28 +0100

    Clean up importd

--- cucumber.eclipse.editor/src/main/java/cucumber/eclipse/launching/CucumberFeatureTabGroup.java
+++ cucumber.eclipse.editor/src/main/java/cucumber/eclipse/launching/CucumberFeatureTabGroup.java
@@ -1,22 +1,12 @@
 package cucumber.eclipse.launching;
 
-import org.eclipse.debug.core.ILaunch;
-import org.eclipse.debug.core.ILaunchConfiguration;
-import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
 import org.eclipse.debug.ui.AbstractLaunchConfigurationTabGroup;
 import org.eclipse.debug.ui.CommonTab;
 import org.eclipse.debug.ui.ILaunchConfigurationDialog;
 import org.eclipse.debug.ui.ILaunchConfigurationTab;
 import org.eclipse.debug.ui.ILaunchConfigurationTabGroup;
-import org.eclipse.jdt.debug.ui.launchConfigurations.JavaArgumentsTab;
 import org.eclipse.jdt.debug.ui.launchConfigurations.JavaClasspathTab;
-import org.eclipse.jdt.debug.ui.launchConfigurations.JavaConnectTab;
 import org.eclipse.jdt.debug.ui.launchConfigurations.JavaJRETab;
-import org.eclipse.jdt.debug.ui.launchConfigurations.JavaMainTab;
-import org.eclipse.jdt.internal.debug.ui.launcher.AbstractJavaMainTab;
-import org.eclipse.jdt.internal.debug.ui.launcher.LocalJavaApplicationTabGroup;
-import org.eclipse.jdt.internal.debug.ui.launcher.SharedJavaMainTab;
-import org.eclipse.swt.widgets.Composite;
 
 public class CucumberFeatureTabGroup 
 	extends AbstractLaunchConfigurationTabGroup
