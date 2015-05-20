d673325209b1e1e5d148c316ce2ffa5e18cd4baa
Ryan Pickett <ryan@hdpe.me>
2015-05-20 23:14:22 +0100

    Extract MarkerManager for re-use and testability.
    Prevents us having to mock what we don't own.

--- cucumber.eclipse.editor/src/main/java/cucumber/eclipse/editor/editors/Editor.java
+++ cucumber.eclipse.editor/src/main/java/cucumber/eclipse/editor/editors/Editor.java
@@ -24,6 +24,7 @@ import org.eclipse.ui.IFileEditorInput;
 import org.eclipse.ui.contexts.IContextService;
 import org.eclipse.ui.editors.text.TextEditor;
 
+import cucumber.eclipse.editor.markers.MarkerManager;
 import cucumber.eclipse.editor.steps.ExtensionRegistryStepProvider;
 
 public class Editor extends TextEditor {
@@ -140,7 +141,8 @@ public class Editor extends TextEditor {
 		IFileEditorInput fileEditorInput = (IFileEditorInput) input;
 		IFile featureFile = fileEditorInput.getFile();
 		GherkinErrorMarker marker = new GherkinErrorMarker(this,
-				new ExtensionRegistryStepProvider(), featureFile, doc);
+				new ExtensionRegistryStepProvider(), new MarkerManager(),
+				featureFile, doc);
 		marker.removeExistingMarkers();
 
 		Parser p = new Parser(marker, false);
--- cucumber.eclipse.editor/src/main/java/cucumber/eclipse/editor/editors/GherkinErrorMarker.java
+++ cucumber.eclipse.editor/src/main/java/cucumber/eclipse/editor/editors/GherkinErrorMarker.java
@@ -9,22 +9,18 @@ import gherkin.formatter.model.Scenario;
 import gherkin.formatter.model.ScenarioOutline;
 import gherkin.formatter.model.Step;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IMarker;
-import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.FindReplaceDocumentAdapter;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.IRegion;
 import org.eclipse.ui.IEditorPart;
-import org.eclipse.ui.texteditor.MarkerUtilities;
 
-import cucumber.eclipse.editor.Activator;
+import cucumber.eclipse.editor.markers.IMarkerManager;
 import cucumber.eclipse.editor.steps.IStepProvider;
 
 /**
@@ -39,22 +35,21 @@ public class GherkinErrorMarker implements Formatter {
 
 	IEditorPart editor;
 	IStepProvider stepProvider;
+	IMarkerManager markerManager;
 	IFile file;
 	IDocument document;
 
-	public GherkinErrorMarker(IEditorPart editor, IStepProvider stepProvider, IFile inputfile, IDocument doc) {
+	public GherkinErrorMarker(IEditorPart editor, IStepProvider stepProvider, IMarkerManager markerManager,
+			IFile inputfile, IDocument doc) {
 		this.editor = editor;
 		this.stepProvider = stepProvider;
+		this.markerManager = markerManager;
 		this.file = inputfile;
 		this.document = doc;
 	}
 
 	public void removeExistingMarkers() {
-		try {
-			file.deleteMarkers(ERROR_ID, true, IResource.DEPTH_ZERO);
-		} catch (CoreException e) {
-			Activator.getDefault().getLog().log(e.getStatus());
-		}
+		markerManager.removeAll(ERROR_ID, file);
 		deleteUnmatchedStepsMarkers(file);
 	}
 
@@ -166,30 +161,25 @@ public class GherkinErrorMarker implements Formatter {
 	@Override
 	public void syntaxError(String state, String event,
 			List<String> legalEvents, String uri, Integer line) {
-		int docline = line - 1;
-		Map<String, Object> map = new HashMap<String, Object>();
-		MarkerUtilities.setLineNumber(map, docline);
-		StringBuffer buf = new StringBuffer();
-		buf.append("Syntax Error: Expected one of ");
+
+		StringBuffer buf = new StringBuffer("Syntax Error: Expected one of ");
 		for (String ev : legalEvents) {
 			buf.append(ev);
 			buf.append(", ");
 		}
 		buf.replace(buf.length() - 3, buf.length(), " but got ");
 		buf.append(event);
-		MarkerUtilities.setMessage(map, buf.toString());
-		map.put(IMarker.SEVERITY, IMarker.SEVERITY_ERROR);
-		map.put(IMarker.LOCATION, file.getFullPath().toString());
+		
 		try {
-			MarkerUtilities.setCharStart(map, document.getLineOffset(docline));
-			MarkerUtilities.setCharEnd(map, document.getLineOffset(docline) + document.getLineLength(docline));
-		}catch(BadLocationException e) {
+			markerManager.add(ERROR_ID,
+					file,
+					IMarker.SEVERITY_ERROR,
+					buf.toString(),
+					line - 1,
+					document.getLineOffset(line - 1),
+					document.getLineOffset(line - 1) + document.getLineLength(line - 1));
+		} catch (BadLocationException e) {
 			// Ignore for now.
-		}
-		try {
-			MarkerUtilities.createMarker(file, map, ERROR_ID);
-		} catch (CoreException ce) {
-			Activator.getDefault().getLog().log(ce.getStatus());
 		}
 	}
 
@@ -203,31 +193,23 @@ public class GherkinErrorMarker implements Formatter {
 	}
 
 	private void deleteUnmatchedStepsMarkers(IFile featureFile) {
-		try {
-			featureFile.deleteMarkers(UNMATCHED_STEP_ERROR_ID, true,
-					IResource.DEPTH_ZERO);
-		} catch (CoreException e1) {
-			e1.printStackTrace();
-		}
+		markerManager.removeAll(UNMATCHED_STEP_ERROR_ID, featureFile);
 	}
 
 	private void markUnmatchedStep(IFile featureFile, IDocument doc,
 			gherkin.formatter.model.Step stepLine) throws BadLocationException,
 			CoreException {
-		Map<String, Object> attributes = new HashMap<String, Object>();
-		attributes.put(IMarker.SEVERITY, IMarker.SEVERITY_WARNING);
-		attributes.put(IMarker.LINE_NUMBER, stepLine.getLine());
-		attributes.put(IMarker.MESSAGE,
-				"Step does not have a matching glue code.");
-
+		
 		FindReplaceDocumentAdapter find = new FindReplaceDocumentAdapter(doc);
 		IRegion region = find.find(0, stepLine.getName(), true, true, false,
 				false);
 
-		attributes.put(IMarker.CHAR_START, region.getOffset());
-		attributes.put(IMarker.CHAR_END,
+		markerManager.add(UNMATCHED_STEP_ERROR_ID,
+				featureFile,
+				IMarker.SEVERITY_WARNING,
+				"Step does not have a matching glue code.",
+				stepLine.getLine() - 1,
+				region.getOffset(),
 				region.getOffset() + region.getLength());
-		MarkerUtilities.createMarker(featureFile, attributes,
-				UNMATCHED_STEP_ERROR_ID);
 	}
 }
